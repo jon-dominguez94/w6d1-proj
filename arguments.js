4a0dcf4297c87d889b7d08ad74a9dce0aeea329a
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-26 12:06:49 -0800

    Split sum implementations into two separate functions

--- arguments.js
+++ arguments.js
@@ -1,6 +1,15 @@
-function sum(...args) {
+function sum1() {
   let sum = 0;
-  // const args = Array.from(arguments);
+  const args = Array.from(arguments);
+  args.forEach(el => {
+    sum += el;
+  });
+  return sum;
+}
+
+
+function sum2(...args) {
+  let sum = 0;
   args.forEach(el => {
     sum += el;
   });
