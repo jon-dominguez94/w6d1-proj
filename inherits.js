be379cfa0341dab9d29f2dbfad70623231b39321
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-26 14:02:26 -0800

    Define inherits function

--- inherits.js
+++ inherits.js
@@ -1,3 +1,6 @@
 Function.prototype.inherits = function(superClass) {
-
+  function Surrogate() {}
+  Surrogate.prototype = superClass.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
 };
