26ec7e10e8d945ff0b18ce9234f3ad0e90c5c8ea
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-26 16:11:38 -0800

    Finish move method

--- asteroids/src/index.js
+++ asteroids/src/index.js
@@ -6,4 +6,7 @@ window.MovingObject = MovingObject;
 document.addEventListener("DOMContentLoaded", function(event) {
   const canvas = document.getElementById("game-canvas");
   const ctx = canvas.getContext("2d");
+
+  window.canvas = canvas;
+  window.ctx = ctx;
 });
--- asteroids/src/moving_object.js
+++ asteroids/src/moving_object.js
@@ -15,7 +15,8 @@ MovingObject.prototype.draw = function (ctx) {
 };
 
 MovingObject.prototype.move = function() {
-
+  this.pos[0] += this.vel[0];
+  this.pos[1] += this.vel[1];
 };
 
 module.exports = MovingObject;
